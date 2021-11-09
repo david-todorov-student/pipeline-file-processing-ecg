7ec884bb9afcff0725003ed2a2e3c6e665d3fd59
David Todorov <todorov_david@yahoo.com>
2021-11-09 16:30:50 +0100

    initial commit

---
+++ PipeFunctions.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionPipelinePrototype
+{
+    public class PipeFunctions
+    {
+        public static IEnumerable<string> getFileNames(string pathToFiles)
+        {
+            if (Directory.Exists(pathToFiles))
+            {
+                return Directory.EnumerateFiles(pathToFiles);
+            }
+            else
+            {
+                throw new DirectoryNotFoundException();
+            }
+        }
+
+        public static IEnumerable<string> sortFileNames(IEnumerable<string> fileNames)
+        {
+            return fileNames.OrderBy(fileName => fileName);
+        }
+
+        public static void readFiles(IEnumerable<string> fileNames, string pathToDirectory)
+        {
+            foreach (var fileName in fileNames)
+            {
+                var fullPath = pathToDirectory + "/" +fileName;
+                if (File.Exists(fullPath))
+                {
+                    File.ReadLines(fullPath);
+                }
+                else
+                {
+                    throw new FileNotFoundException();
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionPipelinePrototype
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pathToFiles = "../../input_files";
+            var fileNames = PipeFunctions.getFileNames(pathToFiles);
+            fileNames = PipeFunctions.sortFileNames(fileNames);
+
+        }
+    }
+}
